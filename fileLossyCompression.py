535828b224d85f2ea46920712ff5f3c02f321b7f
Puneeth <kpuneeth714@gmail.com>
2022-09-25 00:35:15 +0530

    Added support for database and minor bug fixes

--- fileLossyCompression.py
+++ fileLossyCompression.py
@@ -5,10 +5,11 @@ import os
 import sys
 import time
 import yaml
+import sqlite3
 from loguru import logger
 from PIL import Image
 import subprocess
-from get_files import get_files, readConfig
+from get_files import get_files, readConfig, removeSpaces
 
 # log to output.log file
 logger.add("output.log", format="{time} {level} {message}", level="INFO")
@@ -47,13 +48,24 @@ def getTotalSize(file, directory):
         # get size with du command
         size = subprocess.check_output(
             f"du -s {file}", shell=True).decode().split()[0]
-        return int(size)/1024
-    return os.path.getsize(os.path.join(directory, file))/(1024*1024)
+        return round(int(size)/1024, 2)
+    return round(os.path.getsize(os.path.join(directory, file))/(1024*1024), 2)
 
 # it will get list of file paths,destination directory and config file
 
 
-def compressFilesLossy(files, destination, config):
+def addTodatabase(fileName, filePath, destinationName, destinationPath, compressionRatio, connection: sqlite3.Connection, table):
+    # add the file name, file path, destination name, destination path and compression ratio to the database
+    # create table if not exists
+    # add the data to the table with fileName as the primary key
+    query = f"CREATE TABLE IF NOT EXISTS {table} (fileName TEXT PRIMARY KEY, filePath TEXT, destinationName TEXT, destinationPath TEXT, compressionRatio REAL)"
+    connection.execute(query)
+    query = f"INSERT INTO {table} VALUES (\'{fileName}\',\'{filePath}\',\'{destinationName}\',\'{destinationPath}\',{compressionRatio})"
+    logger.info(query)
+    connection.execute(query)
+
+
+def compressFilesLossy(files, root, destination, config, connection: sqlite3.Connection, table):
     # check if destination directory exists and create if not
     if not os.path.exists(destination):
         os.makedirs(destination)
@@ -64,13 +76,17 @@ def compressFilesLossy(files, destination, config):
         # if the file is an image
         if file_type == "image":
             # compress the image
-            compressPIL(file, destination, config)
+            source_file, source_path, destination_folder, dest_file, compression = compressPIL(
+                file, root, destination, config)
         # if the file is a video
         elif file_type == "video":
             # compress the video
-            compressFFMPEG(file, destination, config)
+            source_file, source_path, destination_folder, dest_file, compression = compressFFMPEG(
+                file, root, destination, config)
         # if the file is neither an image nor a video
         else:
+            source_file, source_path, destination_folder, dest_file, compression = os.path.basename(
+                file), os.path.dirname(file), destination, os.path.basename(file), 1
             # log the file type
             logger.info(f"File type: {file_type}")
             # log the file name
@@ -84,9 +100,12 @@ def compressFilesLossy(files, destination, config):
             subprocess.run(f"cp {file} {destination}", shell=True)
             # log the file is copied
             logger.info("File is copied")
+        # write the data to the database
+        addTodatabase(source_file, source_path, dest_file,
+                      destination_folder, compression, connection, table)
 
 
-def compressFFMPEG(source: str, destination: str, config: dict):
+def compressFFMPEG(source: str, root: str, destination: str, config: dict):
     # compressFileLossy will call this function if the source is a video file
     # convet the video to a lower resolution and lower bitrate with given codec in config file
     # and save it in the destination directory
@@ -94,7 +113,11 @@ def compressFFMPEG(source: str, destination: str, config: dict):
     # store file name in source_file variable and store the file path in source_path
     source_file = os.path.basename(source)
     source_path = os.path.dirname(source)
-    dest_file = source_file
+    dest_file = source.replace(root, "")
+    try:
+        os.makedirs(os.path.join(destination, os.path.dirname(dest_file)))
+    except FileExistsError:
+        logger.info("Directory already exists")
     # convert the video to a lower resolution and lower bitrate with given codec in config file
     # and save it in the destination directory
     # use video_format in config file
@@ -104,29 +127,29 @@ def compressFFMPEG(source: str, destination: str, config: dict):
             # if input and output file formats are the same
             logger.info(
                 f"Input and output file formats are the same. Converting {source_file} to {config['video_format']} format")
-            cmd = f"ffmpeg -i {source} -c:v {config['codec']} -b:v {config['bitrate']}  {os.path.join(destination,source_file)}"
         else:
             # if input and output file formats are different
             # remove the extension from the source file and add the video_format from config file
             logger.info(
                 f"Input and output file formats are different. Converting {source_file} to {config['video_format']} format")
-            dest_file = source_file.split(
+            dest_file = dest_file.split(
                 '.')[0] + '.' + config['video_format']
-            cmd = f"ffmpeg -i {source} -c:v {config['codec']} -b:v {config['bitrate']}  {os.path.join(destination,dest_file)}"
+        cmd = f"ffmpeg -v error -i {source} -c:v {config['codec']} -b:v {config['bitrate']}  {os.path.join(destination,dest_file)}"
         subprocess.run(cmd, shell=True)
     # log the source and destination file sizes and compression ratio
     source_size = getTotalSize(source, source_path)
     destination_size = getTotalSize(os.path.join(
         destination, dest_file), destination)
+    compression = round((source_size)/destination_size, 2)
     logger.info(f"Source file size: {source_size} MB")
     logger.info(f"Destination file size: {destination_size} MB")
     logger.info(
-        f"Compression ratio: {(source_size)*100/destination_size} %")
-    # return the destination file path
-    return os.path.join(dest_file, destination)
+        f"Compression ratio: {compression} ")
+    # return
+    return source_file, source_path, destination, dest_file, compression
 
 
-def compressPIL(source: str, destination: str, config: dict):
+def compressPIL(source: str, root: str, destination: str, config: dict):
     # compressFileLossy will call this function if the source is an image file
     # convert the image to a lower quality and save it in the destination directory
     # use given quality ,extension and format in config file
@@ -134,36 +157,41 @@ def compressPIL(source: str, destination: str, config: dict):
     # store file name in source_file variable and store the file path in source_path
     source_file = os.path.basename(source)
     source_path = os.path.dirname(source)
-    dest_file = source_file
+    # dest_file is file with deestination file,removing the root from source and source_file
+    dest_file = source.replace(root, "")
+    try:
+        os.makedirs(os.path.join(destination, os.path.dirname(dest_file)))
+    except FileExistsError:
+        logger.debug("Directory already exists")
+
     # convert the image to a lower quality and save it in the destination directory
     # use given quality ,extension and format in config file
     if not os.path.isfile(os.path.join(destination, dest_file)):
         img = Image.open(source)
-        if config['image_format'] == source_file.split('.')[-1]:
+        if source_file.split('.')[-1] in [config['image_format'], "jpg"]:
             # if input and output file formats are the same
             logger.info(
                 f"Input and output file formats are the same. Converting {source_file} to {config['image_format']} format")
-            img.save(os.path.join(destination, source_file),
-                     format=config['image_format'], quality=config['quality'])
         else:
             # if input and output file formats are different
             # remove the extension from the source file and add the image_format from config file
             logger.info(
                 f"Input and output file formats are different. Converting {source_file} to {config['image_format']} format")
-            dest_file = source_file.split(
+            dest_file = dest_file.split(
                 '.')[0] + '.' + config['image_format']
-            img.save(os.path.join(destination, dest_file),
-                     format=config['image_format'], quality=config['quality'])
+        img.save(os.path.join(destination, dest_file),
+                 format=config['image_format'], quality=config['quality'])
     # log the source and destination file sizes and compression ratio
     source_size = getTotalSize(source, source_path)
     destination_size = getTotalSize(os.path.join(
         destination, dest_file), destination)
+    compression = round((source_size)/destination_size, 2)
     logger.info(f"Source file size: {source_size} MB")
     logger.info(f"Destination file size: {destination_size} MB")
     logger.info(
-        f"Compression ratio: {(source_size)*100/destination_size} %")
-    # return the destination file path
-    return os.path.join(dest_file, destination)
+        f"Compression ratio: {compression}")
+    # return
+    return source_file, source_path, destination, dest_file, compression
 
 
 def main():
@@ -176,20 +204,30 @@ def main():
     config = readConfig(config_file)
     logger.info(f"Source folder: {source}")
     logger.info(f"Destination folder: {destination}")
+    # replace the source names with removing special characters
+    # removeSpaces(source)
     # get file list from source folder
     file_list = get_files(source, condition)
     # get the size of the source folder and log it
+    logger.info("Getting the size of the source folder")
     source_size = getTotalSize(source, source)
     logger.info(f"Source folder size: {source_size} MB")
+    # connect to the database
+    conn = sqlite3.connect(config['database'])
     # call compressFileLossy function
-    compressFilesLossy(file_list, destination, config)
+    compressFilesLossy(file_list, source, destination,
+                       config, conn, table=config['files_table'])
     # get the size of the destination folder and log it
     destination_size = getTotalSize(destination, destination)
+    logger.info(f"Source folder size: {source_size} MB")
     logger.info(f"Destination folder size: {destination_size} MB")
     # log the compression ratio
     logger.info(
-        f"Compression ratio: {(source_size-destination_size)*100/source_size} %")
-
+        f"Compression ratio: {round((source_size)/destination_size,2)}")
+    # commit the changes to the database
+    conn.commit()
+    # close the connection to the database
+    conn.close()
 
 if __name__ == "__main__":
     main()
--- get_files.py
+++ get_files.py
@@ -1,5 +1,8 @@
 import os
 import yaml
+import json
+import pandas as pd
+import sqlite3
 from loguru import logger
 
 
@@ -7,6 +10,11 @@ def readConfig(config):
     # read the config file
     with open(config, "r") as file:
         return yaml.safe_load(file)
+
+
+def replace(file_name: str):
+    # replace all the speciall characteres other than _ with _
+    return file_name.replace(" ", "_").replace("(", "_").replace(")", "_").replace("[", "_").replace("]", "_").replace("{", "_").replace("}", "_").replace("'", "_").replace('"', "_").replace(",", "_").replace(":", "_").replace(";", "_").replace("!", "_").replace("?", "_").replace("&", "_").replace("$", "_").replace("#", "_").replace("@", "_").replace("%", "_").replace("^", "_").replace("*", "_").replace("+", "_").replace("=", "_").replace("|", "_").replace("\\", "_").replace("/", "_").replace("<", "_").replace(">", "_").replace("`", "_").replace("~", "_")
 
 
 def removeSpaces(source: str):
@@ -21,11 +29,29 @@ def removeSpaces(source: str):
             os.chdir("../")
             logger.debug(f"changed to {os.getcwd()}")
             continue
+        new_file_name = replace(file)
         os.rename(os.path.join(source, file), os.path.join(
-            source, file.replace(" ", "")))
-        logger.debug(f"{file} renamed to {file.replace(' ', '')}")
+            source, new_file_name))
+        logger.debug(f"{file} renamed to {new_file_name}")
 
     logger.debug(f"Done removing spaces from {source}")
+
+
+def get_metadata(folder: str):
+    # get the metadata of the file and append it to a json file using exiftool
+    metadata = os.popen(f"exiftool -j {folder} -r").read()
+    metadata = json.loads(metadata)
+    # convert the json file to sql files table file_data
+    convertJSONtoSQL(metadata, "file_data")
+
+
+def convertJSONtoSQL(data: dict, table_name: str):
+    # convert the list of dictionaries to a pandas dataframe and then to a sql file
+    # table with index of keys
+    df = pd.DataFrame(data)
+    # convert the dataframe to a sql file
+    conn = sqlite3.connect("file_data.db")
+    df.to_sql(table_name, conn, if_exists="replace")
 
 
 def get_files(path, condition, isDir=False):
@@ -37,7 +63,7 @@ def get_files(path, condition, isDir=False):
         return files_are
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
-            file_path=os.path.join(root,name)
+            file_path = os.path.join(root, name)
             if condition(file_path):
                 files_are.append(file_path)
     return files_are
@@ -49,3 +75,6 @@ def delete_files(files: list, source: str):
         path = os.path.join(source, file)
         os.remove(path)
         logger.info(f"{path} deleted")
+
+
+# get_metadata("/home/puneeth/Pictures/Files")
